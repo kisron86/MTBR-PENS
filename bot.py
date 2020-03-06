72077c9f7d00d6e37eb9a5a71bb05beba084b2ce
kisron86 <kisron86@gmail.com>
2020-03-06 18:45:45 +0700

    Add python code for telegram

--- bot.py
+++ bot.py
@@ -38,14 +38,43 @@ def handle(msg):
     if command == '/start' or command == '/Kembali' or command == '/Start':
         bot.sendMessage(chat_id, 'Halo Selamat datang Ada yang bisa dibantu?',
                        reply_markup=ReplyKeyboardMarkup(
-                       keyboard=[[KeyboardButton(text="/Voltage Motor"), KeyboardButton(text="/Voltage PC")],[KeyboardButton(text="/Volume"), KeyboardButton(text="/Power Off")]]))
-    elif command == '/Volume':
-        for row in records:
-            id = row[0]
-            nama = row[1]
-            alamat = row[3]
+                       keyboard=
+                       [[KeyboardButton(text="/Voltage_Motor"), 
+                       KeyboardButton(text="/Voltage_PC")],
+                       [KeyboardButton(text="/Estimation_Motor"), 
+                       KeyboardButton(text="/Estimation_PC")],
+                       [KeyboardButton(text="/Trash_Level"), 
+                       KeyboardButton(text="/Power_Off")]]))
 
-        bot.sendMessage(chat_id, alamat)                            
+        
+    elif command == '/Voltage_Motor':
+        for row in records:
+                v_motor = str(row[1])
+        bot.sendMessage(chat_id, "Voltage of battery motor is " + v_motor + " Volt");
+    elif command == '/Voltage_PC':
+        for row in records:
+                v_pc = str(row[2])
+        bot.sendMessage(chat_id, "Voltage of battery PC is " + v_pc + " Volt");
+    elif command == '/Estimation_Motor':
+        for row in records:
+                t_est_motor = str(row[3])
+        bot.sendMessage(chat_id, "Estimation working time of battery motor is " + t_est_motor + " Minutes");
+    elif command == '/Estimation_PC':
+        for row in records:
+                t_est_pc = str(row[4])
+        bot.sendMessage(chat_id, "Estimation working time of battery PC is " + t_est_pc + " Minutes");
+    elif command == '/Trash_Level':
+        for row in records:
+                trash_level = str(row[5])
+        bot.sendMessage(chat_id, "The level of trash is " + trash_level + " %");
+    elif command == '/Power_Off':
+        for row in records:
+                status = row[6]
+                if status == 1:
+                    bot.sendMessage(chat_id, "Are you sure to shutdown the robot? Y/T");
+    elif command == 'Y':
+        bot.sendMessage(chat_id, "The robot is stopped working");
+                    
 bot = telepot.Bot('465769589:AAFg10QLqp7XKf_2DpGgTfCWPzCtiOe0Yus')
 bot.message_loop(handle)
 print ('Menunggu Perintah ...')
---
+++ build/face_detection/CMakeFiles/face_detection_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/face_detection_gencfg"
+  "/home/kisron/catkin_workspace/devel/include/face_detection/face_detConfig.h"
+  "/home/kisron/catkin_workspace/devel/share/face_detection/docs/face_detConfig.dox"
+  "/home/kisron/catkin_workspace/devel/share/face_detection/docs/face_detConfig-usage.dox"
+  "/home/kisron/catkin_workspace/devel/lib/python2.7/dist-packages/face_detection/cfg/face_detConfig.py"
+  "/home/kisron/catkin_workspace/devel/share/face_detection/docs/face_detConfig.wikidoc"
+  "/home/kisron/catkin_workspace/devel/include/face_detection/face_det_dlibConfig.h"
+  "/home/kisron/catkin_workspace/devel/share/face_detection/docs/face_det_dlibConfig.dox"
+  "/home/kisron/catkin_workspace/devel/share/face_detection/docs/face_det_dlibConfig-usage.dox"
+  "/home/kisron/catkin_workspace/devel/lib/python2.7/dist-packages/face_detection/cfg/face_det_dlibConfig.py"
+  "/home/kisron/catkin_workspace/devel/share/face_detection/docs/face_det_dlibConfig.wikidoc"
+  "/home/kisron/catkin_workspace/devel/include/face_detection/face_det_cudaConfig.h"
+  "/home/kisron/catkin_workspace/devel/share/face_detection/docs/face_det_cudaConfig.dox"
+  "/home/kisron/catkin_workspace/devel/share/face_detection/docs/face_det_cudaConfig-usage.dox"
+  "/home/kisron/catkin_workspace/devel/lib/python2.7/dist-packages/face_detection/cfg/face_det_cudaConfig.py"
+  "/home/kisron/catkin_workspace/devel/share/face_detection/docs/face_det_cudaConfig.wikidoc"
+  "/home/kisron/catkin_workspace/devel/include/face_detection/face_trackConfig.h"
+  "/home/kisron/catkin_workspace/devel/share/face_detection/docs/face_trackConfig.dox"
+  "/home/kisron/catkin_workspace/devel/share/face_detection/docs/face_trackConfig-usage.dox"
+  "/home/kisron/catkin_workspace/devel/lib/python2.7/dist-packages/face_detection/cfg/face_trackConfig.py"
+  "/home/kisron/catkin_workspace/devel/share/face_detection/docs/face_trackConfig.wikidoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/face_detection_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
